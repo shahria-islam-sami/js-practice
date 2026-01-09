04825ad93a5fa789cc4cd7a688ce4158b973a613
Shahria-Islam-Sami <mohdshahriaislamsami@gmail.com>
2026-01-09 21:57:19 +0600

    argument pass in function

---
+++ argument-function.js
@@ -0,0 +1,7 @@
+function difference(num1, num2){
+    const diff = num1 - num2;
+    console.log(num1, num2 , 'difference is : ', diff);
+}
+const fatherAge = 60;
+const  sonAge = 20;
+difference(fatherAge,sonAge);
