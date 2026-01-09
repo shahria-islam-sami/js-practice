0947b69e216d3576111c48e9183205b4df4b1883
Shahria-Islam-Sami <mohdshahriaislamsami@gmail.com>
2026-01-09 22:58:12 +0600

    another new array is declared for no duplicate item and give a condition to store the unique element in the new array

--- duplicate-array.js
+++ duplicate-array.js
@@ -1,8 +1,11 @@
 function noDuplicate(personList){
     for(const person of personList){
+           const uniques = [];
         console.log(person);
+        if(uniques.includes(person) === false){
+            uniques.push(person);
+        }
     }
-
 }
 const personList = ['abul','kabul', 'cabul','abul', 'babul','kabul', 'babul','bulbul','dabul','kabul', 'dabul','babul']
 noDuplicate(personList);
